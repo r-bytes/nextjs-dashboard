31a6bae7131520c66d3e0ff9711ed29b1e91adda
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-11-20 20:55:39 +0100

    feat: streaming, suspense, skeleton fallbacks

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -3,7 +3,6 @@ import { CustomerField, CustomersTable, InvoiceForm, InvoicesTable, LatestInvoic
 import { formatCurrency } from "./utils"
 import { unstable_noStore as noStore } from "next/cache"
 
-
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
@@ -13,12 +12,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log("Fetching revenue data...")
+    await new Promise((resolve) => setTimeout(resolve, 3000))
 
     const data = await sql<Revenue>`SELECT * FROM revenue`
 
-    console.log('Data fetch complete after 3 seconds.');
+    console.log("Data fetch complete after 3 seconds.")
 
     return data.rows
   } catch (error) {
